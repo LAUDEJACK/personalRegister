88824e66bcf1f8ce87ecbfa7ce3b7fdaf44b076a
Laude Cardoso <laudecirjrcardoso@gmail.com>
2024-04-19 19:35:31 -0300

    init commit

---
+++ src/test/java/projetojava/br/personRegister/PersonRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package projetojava.br.personRegister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
